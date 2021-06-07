a779babc83189ddaa5cd6ae35b8395537f72385c
Rakshin <ykatirinka@mail.ru>
2021-06-08 00:14:55 +0300

    4.9. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const TITLES = ['Уютная квартирка с видом на старую площадь', 'Дом в викторианском стиле с настоящим дворецким','Тропическое бунгало в центре современного мегаполиса', 'Двухэтажное бунгало на сваях и с прозрачным полом', 'Дворец в стиле венецианское барокко'];
 const DESCRIPTION = ['10 минут от Метро', 'Окна выходят во двор', 'Ди­зайн квар­ти­ры вы­пол­нен в классическом сти­ле', 'Ис­поль­зо­вани­ем ка­чес­твен­ных и современных ма­тери­алов'];
 const TIME_CHECK = ['12:00', '13:00', '14:00'];
 const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/duonguyen-8LrGtIxxa4w.randomIdxpg', 'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.randomIdxpg', 'https://assets.htmlacademy.ru/content/intensive/randomIdxavascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.randomIdxpg'];
 const AVATAR = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
 
-const ADVERTISEMENT_QUANTITY = 1;
-const SIMILAR_ADVERTISEMENT_COUNT = 4;
+const SIMILAR_COUNT = 4;
 
 function getRandomPositiveInteger (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
@@ -26,11 +26,9 @@ function getRandomPositiveFloat (min, max, digits = 1) {
   return result.toFixed(digits);
 }
 
-const getRandomArrayElement = (elements) => {
-  return elements[_.random(0, elements.length - 1)];
-};
+const getRandomArrayElement = (items) => items[getRandomPositiveInteger(0, items.length - 1)];
 
-// функция для создания объявления;
+
 const creatAdvertisement = () => {
   const COORDINATES = {
     lat: getRandomPositiveFloat(35.65, 35.7, 5),
@@ -42,7 +40,7 @@ const creatAdvertisement = () => {
       avatar: getRandomArrayElement(AVATAR),
     },
     offer: {
-      title: 'Лучшие апартаменты!',
+      title: getRandomArrayElement(TITLES),
       address: `${COORDINATES.lat}, ${COORDINATES.lng}`,
       price: getRandomPositiveInteger(0, 10000),
       type: getRandomArrayElement(TYPES),
@@ -61,4 +59,6 @@ const creatAdvertisement = () => {
   };
 };
 
-const similarAdvertisement = new Array(ADVERTISEMENT_QUANTITY).fill(null).map(() => creatAdvertisement());
+const similarAdvertisement = new Array(SIMILAR_COUNT).fill(null).map(() => creatAdvertisement());
+
+similarAdvertisement.slice();
